ebd06f99c76aa99ab536f7c5ef9b0c8baacbb8c5
AbuBakar <muhammadabubakar19187@gmail.com>
2025-07-10 22:39:25 +0500

    Install Postgres, Docker, Create Schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
 const Homepage = () => {
+  useEffect(()=>{
+    redirect('/dashboard/admin')
+  },[])
   return (
     <div className=''>Homepage</div>
   )
